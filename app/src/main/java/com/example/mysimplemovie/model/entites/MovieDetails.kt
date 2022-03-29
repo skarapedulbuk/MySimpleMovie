7be436eb547f66aa1fb54f720b59979e6439c79d
skarapedulbuk <voronezh2@gmail.com>
2022-03-29 23:29:20 +0300

    Добавлен простой UI

--- app/src/main/java/com/example/mysimplemovie/model/entites/MovieDetails.kt
+++ app/src/main/java/com/example/mysimplemovie/model/entites/MovieDetails.kt
@@ -1,10 +1,10 @@
 package com.example.mysimplemovie.model.entites
 
 data class MovieDetails(
-    val movie: Movie = getDefaulMovie(),
+    val movie: Movie = getDefaultMovie(),
     val overview: String = "Default Overview",
     val voteAverage: Double = 0.0,
     val releaseDate: String = "2000-01-01"
 )
 
-fun getDefaulMovie() = Movie("Title Default")
+fun getDefaultMovie() = Movie("Title Default")
--- app/src/main/java/com/example/mysimplemovie/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mysimplemovie/ui/main/MainFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.mysimplemovie.R
 import com.example.mysimplemovie.databinding.MainFragmentBinding
+import com.example.mysimplemovie.model.AppState
+import com.example.mysimplemovie.model.entites.MovieDetails
+import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel // откуда скопировал?
 
 class MainFragment : Fragment() {
@@ -30,6 +34,37 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val observer = Observer<AppState> { renderData(it) }
+        viewModel.liveData.observe(viewLifecycleOwner, observer)
+        viewModel.getMovieDetails()
+    }
+
+    private fun renderData(appState: AppState) = with(binding) {
+        when (appState) {
+            is AppState.Success -> {
+                val movieData = appState.movieData
+                progressBar.visibility = View.GONE
+                itemGroup.visibility = View.VISIBLE
+                setData(movieData)
+            }
+            is AppState.Loading -> {
+                itemGroup.visibility = View.INVISIBLE
+                progressBar.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                progressBar.visibility = View.GONE
+                itemGroup.visibility = View.INVISIBLE
+                Snackbar
+                    .make(main, R.string.error, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.reload) { viewModel.getMovieDetails() }
+                    .show()
+            }
+        }
+    }
+
+    private fun setData(movieData: MovieDetails) = with(binding) {
+        itemTextView.text = movieData.movie.title
+        itemImage.setImageResource(R.drawable.w200)
     }
 
     override fun onDestroyView() {

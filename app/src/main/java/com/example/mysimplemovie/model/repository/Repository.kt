b1d8b146e917b36e58a12211972321b66628ccb7
skarapedulbuk <voronezh2@gmail.com>
2022-04-03 15:16:26 +0300

    Приведено к одной сущности Movie: Parcelable, в репозиторий добавлен метод получения локального списка фильмов

--- app/src/main/java/com/example/mysimplemovie/model/repository/Repository.kt
+++ app/src/main/java/com/example/mysimplemovie/model/repository/Repository.kt
@@ -1,7 +1,9 @@
 package com.example.mysimplemovie.model.repository
 
+import com.example.mysimplemovie.model.entites.Movie
 import com.example.mysimplemovie.model.entites.MovieDetails
 
 interface Repository {
-    fun getMovieDetailsFromLocalStorage():MovieDetails
+    fun getMovieDetailsFromLocalStorage(): MovieDetails
+    fun getMoviesListFromLocal(): List<Movie>
 }
--- app/src/main/java/com/example/mysimplemovie/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/mysimplemovie/model/repository/RepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.example.mysimplemovie.model.repository
 
+import com.example.mysimplemovie.model.entites.Movie
 import com.example.mysimplemovie.model.entites.MovieDetails
+import com.example.mysimplemovie.model.entites.getListOne
 
 class RepositoryImpl: Repository {
     override fun getMovieDetailsFromLocalStorage() = MovieDetails()
+    override fun getMoviesListFromLocal(): List<Movie> = getListOne()
 }
